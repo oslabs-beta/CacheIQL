96a43498495f9d72f2f579ab2939b81b0e546927
Gabriella Davoudpour <gdavoudpour@gmail.com>
2024-12-05 19:35:11 -0800

    created functions for errors, generating key, and custom fetch call

--- cacheiql-client/src/index.ts
+++ cacheiql-client/src/index.ts
@@ -1,5 +1,56 @@
-function test(testie: string): string {
-  return testie;
+import { ClientErrorType } from "./types";
+
+const fs = require('fs');
+const graphql = require("graphql");
+
+// import client error type
+const { ClientErrorType } = require('./types');
+
+// client error function
+const createClientError = (message: string): ClientErrorType => {
+  return {
+    log: message,
+    status: 400,
+    message: { err: 'Something went wrong in cacheqlIt fetch'}
+  }
 }
 
-module.exports = test;
+
+
+// function that creates unique key for each query and response
+const generateKey = (query: string, variables: object): string => {
+  return `${query}_${JSON.stringify(variables)}`
+}
+
+// function that makes fetch
+const cacheiqIt = async (endpoint: string, query: string, variables?: object ): Promise<string> => {
+  try {
+    // FINISH THIS PART
+  } catch(err) {
+    if (err instanceof Error) {
+      console.log('something wrong with fetching query!')
+      return err.message;
+    }
+  }
+  // FIX THIS!!!!
+  return 'temp'
+}
+
+// create function that handles saving data to local storage
+const checkAndSaveToCache = (query: string, variables: object): void => {
+  const key = generateKey(query, variables);
+  const data = localStorage.getItem(query);
+  if (data) {
+    console.log('found data!');
+    return;
+  } else {
+    // invoke function that makes graphql query
+    // localStorage.setItem()
+  }
+
+}
+
+// function that checks if query is stored in local storage
+  // if so, grab that response and return it
+
+
