ec3c5a6185ced9a70de24fe7f560e17d636946f8
Gabriella Davoudpour <gdavoudpour@gmail.com>
2024-12-10 19:52:33 -0800

    Began Mutation function creation and associated handlers

--- cacheiql-client/src/index.ts
+++ cacheiql-client/src/index.ts
@@ -1,6 +1,9 @@
 import { ClientErrorType } from './types';
+import gql from 'graphql-tag';
+import { visit } from 'graphql';
 import fs from 'fs';
 import graphql from 'graphql';
+import { DocumentNode } from 'graphql';
 
 // import client error type
 // you want to import not export a type because it leads to more errors
@@ -53,6 +56,12 @@ export const cacheiqIt = async (
   }
 };
 
+export const mutationTypes: object = {
+  delete: ['delete', 'remove'],
+  update: ['update', 'edit'],
+  create: ['create', 'add', 'new', 'make']
+}
+
 // create function that handles saving data to local storage
 export const checkAndSaveToCache = (
   // potentially add type parameter to check for mutations
@@ -62,23 +71,64 @@ export const checkAndSaveToCache = (
 ): string | void | boolean => {
   const key = generateKey(query, variables);
   const data = localStorage.getItem(query);
+
+  // add checker to see if query type is a mutation
+  try {
+    // parse query using graphql-tag feature
+    const parsedQuery: DocumentNode = gql`${query}`;
+    // check if mutation is present in parsed query
+    // check if any of the objects in definitions array has a mutation, return true if so
+    const containsMutation: boolean = parsedQuery.definitions.some((definition) => 
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'mutation'
+    )
+
+    if (containsMutation) {
+      // parse query to extract name 
+    let mutationName: string | null = null;
+    // traverse through AST 
+    visit(parsedQuery, {
+      // this should be invoked whenever the visit function encounters an operation defintion node
+      // here, we create operation defintion key with associated method which is operationdefinition(node)
+      OperationDefinition(node) {
+        // if the node is a mutation and the value of the name property in node is defined
+        if (node.operation === 'mutation' && node.name) {
+          // set mutationName to the value of the name key in the node
+          mutationName = node.name.value;
+        }
+      }
+    })
+
+      // target first keyword in typeofmutation to determine type
+      // check to see what type of mutation
+      // invoke respective handler function
+    }
+  } catch (err) {
+    console.log('something went wrong when checking for mutations!')
+  }
+
   if (data) {
     console.log('found data!');
     return true;
-  } else if (!data && response) {
+  } 
+  // potentially add another else if to check if type is a mutation
+    // if so, invoke mutation updater function
+  else if (!data && response) {
     localStorage.setItem(
       query,
       JSON.stringify(response)
     );
     return undefined;
   } 
-  // potentially add another else if to check if type is a mutation
-    // if so, invoke mutation updater function
   else {
     return false;
   }
 };
 
+// function to handle mutation change update query/response
+export const mutationHandler = (key: string) => {
+  if (localStorage.hasOwnProperty(key)) {
 
-// function to check for mutation change / update query/response
+  }
+}
 
