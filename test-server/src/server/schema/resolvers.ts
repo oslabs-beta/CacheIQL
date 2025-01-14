c0335f391550e73d3bdd37ace362635bd32933c1
VaseanAnnin <164922449+VaseanAnnin@users.noreply.github.com>
2025-01-13 20:38:21 -0500

    Revert "bug: Feature/schema hiding "

--- test-server/src/server/schema/resolvers.ts
+++ test-server/src/server/schema/resolvers.ts
@@ -1,14 +1,7 @@
 import { query } from 'express';
-import { GraphQLResolveInfo } from 'graphql';
-import { GraphQLArgs } from 'graphql';
+
 const db = require('../models/starWarsModels');
 const schema = require('./schema');
-
-//What context does example
-const context = {
-  id: 1,
-  name: 'donald',
-};
 //specifies the data types and Queries
 //an object that contains resolver functions
 //the keys correspond to the field names in the Query type
@@ -16,22 +9,12 @@ const context = {
 module.exports = {
   //using postgre sql this resolver selects all of the people from the database and returns them
   //Query: {
-  people: async (
-    parent: any,
-    args: any,
-    //context does not exist on native graphql
-    //context: any,
-    info: GraphQLResolveInfo
-  ): Promise<object> => {
+  people: async (): Promise<object> => {
     //query
-    // console.log(info);
-    // console.log(parent);
-    //console.log('info', context);
     const query = 'SELECT * FROM people';
     //request
     const results = await db.query(query);
     //response being returned is in the shape of an array
-    // console.log('Returned people data:', results.rows);
     return results.rows;
   },
 
