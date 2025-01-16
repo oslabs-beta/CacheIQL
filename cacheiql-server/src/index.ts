26f3c860b4c27929be345f06dc3867fc3acda48b
Vasean Annin <vaseanannin@Vaseans-MacBook-Air.local>
2025-01-15 21:20:31 -0500

    Fixed server side package

--- cacheiql-server/src/index.ts
+++ cacheiql-server/src/index.ts
@@ -2,23 +2,50 @@ import { setCacheQuery, getCachedQuery, invalidateCache } from './cacheManager';
 import { GraphQLResolveInfo } from 'graphql';
 
 export const cacheMiddleware =
-  (resolve: any) =>
-  async (root: any, args: any, context: any, info: GraphQLResolveInfo) => {
-    const key = `${info.parentType.name}:${info.fieldName}:${JSON.stringify(
-      args
-    )}`;
+  (
+    resolve: (
+      parent: any,
+      args: any,
+      info: GraphQLResolveInfo,
+      context?: any
+    ) => Promise<any>
+  ) =>
+  async (
+    parent: any,
+    args: any,
+    info: GraphQLResolveInfo,
+    context?: any
+  ): Promise<any> => {
+    if (!info) {
+      console.error('Missing GraphQlResolveInfo in cacheMiddleware');
 
-    // Check the cache
-    const cachedData = await getCachedQuery(key);
-    if (cachedData) {
-      console.log(`Cache hit for ${key}`);
-      return cachedData;
+      return await resolve(parent, args, context, info as GraphQLResolveInfo);
     }
+    const parentType = info.parentType.name
+      ? info.parentType.name
+      : info.parentType;
+    // Check the cache
+    // if (info.parentType.name) {
+    //   const parentType = info.parentType.name;
+    // } else {
+    //   const parentType = info.parentType;
+    // }
+    const key = `${info.parentType.name}:${info.fieldName}:${args}`;
 
-    // Execute resolver and cache the result
-    const result = await resolve(root, args, context, info);
-    await setCacheQuery(key, result);
-    return result;
+    try {
+      const cachedData = await getCachedQuery(key);
+      if (cachedData) {
+        console.log(`Cache hit for ${key}`);
+
+        return cachedData;
+      }
+      console.log(`Cache miss for ${key}`);
+      const result = await resolve(parent, args, context, info);
+      await setCacheQuery(key, result);
+      return result;
+    } catch (error) {
+      console.error(error);
+    }
   };
 
 export const invalidateCacheForMutation = async (
